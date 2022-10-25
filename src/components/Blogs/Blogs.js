3c1c1ff81165346bc2e386e80d6fa4f50841f021
neasher <neasherahmed75@gmail.com>
2022-10-26 02:43:01 +0600

    blogs added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import Accordion from 'react-bootstrap/Accordion';
 
 const Blogs = () => {
     return (
-        <div>
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>what is the main perspose of react router?</Accordion.Header>
-                    <Accordion.Body>
-                        React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>how does context api work?</Accordion.Header>
-                    <Accordion.Body>
-                        The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>what is useref in react?</Accordion.Header>
-                    <Accordion.Body>
-                        Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with, React will set its .current property to the corresponding DOM node whenever that node changes
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+        <div className="container">
+            <div className="row d-flex justify-content-center align-items-center">
+                <div className="col-lg-6 text-center">
+                    <h2 className='text-center mt-4'>BLOGS</h2>
+                </div>
+            </div>
+            <div className="row mt-4">
+                <div className="col-lg-4">
+                    <h4>what is the main perspose of react router?</h4>
+                    <p>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL</p>
+                </div>
+                <div className="col-lg-4">
+                    <h4>how does context api work?</h4>
+                    <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+                </div>
+                <div className="col-lg-4">
+                    <h4>what is useref in react?</h4>
+                    <p>Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with, React will set its .current property to the corresponding DOM node whenever that node changes</p>
+                </div>
+            </div>
         </div>
     );
 };
